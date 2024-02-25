d398cb8ad3edd00747341e3481a3b0db3092e524
Harsh Shukla <107511647+harshshukla07@users.noreply.github.com>
2024-02-25 11:52:19 +0530

    Time: 210 ms (33.33%) | Memory: 58.7 MB (50.00%) - LeetSync

--- 3325-find-the-largest-area-of-square-inside-two-rectangles/find-the-largest-area-of-square-inside-two-rectangles.cpp
+++ 3325-find-the-largest-area-of-square-inside-two-rectangles/find-the-largest-area-of-square-inside-two-rectangles.cpp
@@ -1,13 +1,13 @@
 class Solution {
 public:
-    long long largestSquareArea(vector<vector<int>>& nums1, vector<vector<int>>& nums2) {
+    long long largestSquareArea(vector<vector<int>>& bottomLeft, vector<vector<int>>& topRight) {
         long long area=0;
-        for(int i=0;i<nums1.size();i++){
-            for(int j=i+1;j<nums1.size();j++){
-                long long minimum_x = max(nums1[i][0], nums1[j][0]);
-                long long maximum_x = min(nums2[i][0], nums2[j][0]);
-                long long minimum_y = max(nums1[i][1], nums1[j][1]);
-                long long maximum_y = min(nums2[i][1], nums2[j][1]);
+        for(int i=0;i<bottomLeft.size();i++){
+            for(int j=i+1;j<bottomLeft.size();j++){
+                long long minimum_x = max(bottomLeft[i][0], bottomLeft[j][0]);
+                long long maximum_x = min(topRight[i][0], topRight[j][0]);
+                long long minimum_y = max(bottomLeft[i][1], bottomLeft[j][1]);
+                long long maximum_y = min(topRight[i][1], topRight[j][1]);
                 
                 if(minimum_x<maximum_x && minimum_y<maximum_y){
                     long long s = min(maximum_x-minimum_x, maximum_y-minimum_y);
