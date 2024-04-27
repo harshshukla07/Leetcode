aab0d3f8eb0531b61fe5cef194a1153ee1dd8281
Harsh Shukla <107511647+harshshukla07@users.noreply.github.com>
2024-04-27 23:47:23 +0530

    Time: 183 ms (30.74%) | Memory: 122.8 MB (47.73%) - LeetSync

--- 234-palindrome-linked-list/palindrome-linked-list.cpp
+++ 234-palindrome-linked-list/palindrome-linked-list.cpp
@@ -9,40 +9,46 @@
  * };
  */
 class Solution {
+
+private:
+    ListNode* reverseLL(ListNode* head){
+        if(head==NULL || head->next==NULL){
+            return head;
+        }
+
+        ListNode* newHead = reverseLL(head->next);
+        ListNode* front = head->next;
+        front->next = head;
+        head->next = NULL;
+        return newHead;
+    }
+
 public:
     bool isPalindrome(ListNode* head) {
-        
-        ListNode *slow = head, *fast = head;
-        while(fast!=NULL && fast->next !=NULL){
+        if(head==NULL || head->next==NULL){
+            return true;
+        }
+        ListNode* slow = head;
+        ListNode* fast = head;
+
+        while(fast->next && fast->next->next){
             slow = slow->next;
             fast = fast->next->next;
         }
 
-        if(fast != NULL && fast->next == NULL){
-            slow = slow->next;
-        }
-        
-        ListNode *prev = NULL;
-        ListNode *temp = NULL;
-        while(slow != NULL && slow->next != NULL){
-            temp = slow->next;
-            slow->next = prev;
-            prev = slow;
-            slow = temp;
-        }
-        if(slow != NULL){
-            slow->next = prev;
-        }
-        
-        fast = head;
-        while(slow && fast){
-            if(slow->val != fast->val){
+        ListNode* newHead = reverseLL(slow->next);
+
+        ListNode* first = head;
+        ListNode* second = newHead;
+        while(second){
+            if(first->val!=second->val){
+                reverseLL(newHead);
                 return false;
             }
-            slow = slow->next;
-            fast = fast->next;
+            first = first->next;
+            second = second->next;
         }
+        reverseLL(newHead);
         return true;
-
     }
 };
