ba03de7bb67491b624bd007e5d9d8b4712a1ca51
Harsh Shukla <107511647+harshshukla07@users.noreply.github.com>
2024-05-25 12:53:55 +0530

    Time: 0 ms (100.00%) | Memory: 8.9 MB (77.56%) - LeetSync

---
+++ 140-word-break-ii/word-break-ii.cpp
@@ -0,0 +1,37 @@
+class Solution {
+public:
+    vector<string> wordBreak(string s, vector<string>& wordDict) {
+        unordered_map<int, vector<string>> dp;
+
+        for (int startIdx = s.size(); startIdx >= 0; startIdx--) {
+            vector<string> validSentences;
+            for (int endIdx = startIdx; endIdx < s.size(); endIdx++) {
+                string currentWord = s.substr(startIdx, endIdx - startIdx + 1);
+
+                if (isWordInDict(currentWord, wordDict)) {
+                    if (endIdx == s.size() - 1) {
+                        validSentences.push_back(currentWord);
+                    } else {
+                        vector<string> sentencesFromNextIndex = dp[endIdx + 1];
+                        for (const auto& sentence : sentencesFromNextIndex) {
+                            validSentences.push_back(currentWord + " " + sentence);
+                        }
+                    }
+                }
+            }
+            dp[startIdx] = validSentences;
+        }
+
+        return dp[0];
+    }
+
+private:
+    bool isWordInDict(const string& word, const vector<string>& wordDict) {
+        for (const auto& dictWord : wordDict) {
+            if (dictWord == word) {
+                return true;
+            }
+        }
+        return false;
+    }
+};
