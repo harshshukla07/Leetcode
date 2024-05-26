c36b4551c84620690e1dcb23e4dfe918a1b0fdfb
Harsh Shukla <107511647+harshshukla07@users.noreply.github.com>
2024-05-26 13:29:55 +0530

    Time: 1398 ms (11.79%) | Memory: 420.4 MB (22.05%) - LeetSync

--- 552-student-attendance-record-ii/student-attendance-record-ii.cpp
+++ 552-student-attendance-record-ii/student-attendance-record-ii.cpp
@@ -3,30 +3,25 @@ public:
     int checkRecord(int n) {
         int MOD = 1000000007;
         vector<vector<int>> dpCurrState = vector<vector<int>>(2, vector<int>(3, 0));
-
         vector<vector<int>> dpNextState = vector<vector<int>>(2, vector<int>(3, 0));
 
-        // Base case: there is 1 string of length 0 with zero 'A' and zero 'L'.
         dpCurrState[0][0] = 1;
 
-        // Iterate on smaller sub-problems and use the current smaller sub-problem 
-        // to generate results for bigger sub-problems.
         for (int len = 0; len < n; ++len) {
             for (int totalAbsences = 0; totalAbsences <= 1; ++totalAbsences) {
                 for (int consecutiveLates = 0; consecutiveLates <= 2; ++consecutiveLates) {
-                    // Store the count when 'P' is chosen.
                     dpNextState[totalAbsences][0] = (
                         dpNextState[totalAbsences][0] + 
                         dpCurrState[totalAbsences][consecutiveLates]
                     ) % MOD;
-                    // Store the count when 'A' is chosen.
+
                     if (totalAbsences < 1) {
                         dpNextState[totalAbsences + 1][0] = (
                             dpNextState[totalAbsences + 1][0] + 
                             dpCurrState[totalAbsences][consecutiveLates]
                         ) % MOD;
                     }
-                    // Store the count when 'L' is chosen.
+                    
                     if (consecutiveLates < 2) {
                         dpNextState[totalAbsences][consecutiveLates + 1] = (
                             dpNextState[totalAbsences][consecutiveLates + 1] + 
@@ -36,13 +31,11 @@ public:
                 }
             }
             
-            // Next state sub-problems will become current state sub-problems in next iteration.
             dpCurrState = dpNextState;
-            // Next state sub-problem results will reset to zero.
+            
             dpNextState = vector<vector<int>>(2, vector<int>(3, 0));
         }
 
-        // Sum up the counts for all combinations of length 'n' with different absent and late counts.
         int count = 0;
         for (int totalAbsences = 0; totalAbsences <= 1; ++totalAbsences) {
             for (int consecutiveLates = 0; consecutiveLates <= 2; ++consecutiveLates) {
