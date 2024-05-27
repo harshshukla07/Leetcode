6126777ed912775394244fa66b2ccb97ba53908b
Harsh Shukla <107511647+harshshukla07@users.noreply.github.com>
2024-05-27 15:55:21 +0530

    Time: 10 ms (18.62%) | Memory: 16.9 MB (49.24%) - LeetSync

--- 733-flood-fill/flood-fill.cpp
+++ 733-flood-fill/flood-fill.cpp
@@ -1,27 +1,27 @@
 class Solution {
 
 private:
-    void dfs(vector<vector<int>>& image, int sr, int sc, int color, vector<vector<int>>& ans, vector<int> drow, vector<int> dcol, int initialColor){
+    void dfs(vector<vector<int>>& image, int sr, int sc, int color, vector<vector<int>>& ans, vector<int> dx, vector<int> dy, int ini){
         ans[sr][sc] = color;
         int n = image.size();
         int m = image[0].size();
         for(int i=0;i<4;i++){
-            int row = sr + drow[i];
-            int col = sc + dcol[i];
-
-            if(row>=0 && row<n && col>=0 && col<m && image[row][col]==initialColor && ans[row][col]!=color ){
-                dfs(image,row,col,color,ans,drow,dcol,initialColor);
+            int row = sr + dx[i];
+            int col = sc + dy[i];
+            if(row>=0 && row<n && col>=0 && col<m && image[row][col]==ini && ans[row][col]!=color){
+                dfs(image,row,col,color,ans,dx,dy,ini);
             }
         }
     }
 
 public:
     vector<vector<int>> floodFill(vector<vector<int>>& image, int sr, int sc, int color) {
-        int initialColor = image[sr][sc];
         vector<vector<int>> ans = image;
-        vector<int> drow = {0,1,0,-1};
-        vector<int> dcol = {1,0,-1,0};
-        dfs(image,sr,sc,color,ans,drow,dcol,initialColor);
+        int initialcolor = image[sr][sc];
+        vector<int> dx = {0,1,0,-1};
+        vector<int> dy = {1,0,-1,0};
+        dfs(image,sr,sc,color,ans,dx,dy,initialcolor);
         return ans;
+
     }
 };
