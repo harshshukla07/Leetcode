c015c92cd54503350b3e7e7489f5be2cd7e213ea
Harsh Shukla <107511647+harshshukla07@users.noreply.github.com>
2024-05-27 14:55:45 +0530

    Time: 9 ms (20.61%) | Memory: 16.1 MB (42.88%) - LeetSync

--- 1036-rotting-oranges/rotting-oranges.cpp
+++ 1036-rotting-oranges/rotting-oranges.cpp
@@ -2,15 +2,15 @@ class Solution {
 
 public:
     int orangesRotting(vector<vector<int>>& grid) {
-        int fresh = 0,time=0,rotten=0;
+        int fresh = 0,rotten=0;
         int n = grid.size();
         int m = grid[0].size();
-        queue<pair<int,int>>q;
+        queue<pair<pair<int,int>,int>>q;
         vector<vector<int>>vis(n,vector<int>(m,0));
         for(int i=0;i<n;i++){
             for(int j=0;j<m;j++){
                 if(grid[i][j]==2){
-                    q.push({i,j});
+                    q.push({{i,j},0});
                     vis[i][j] = 2;
     
                 }
@@ -25,34 +25,25 @@ public:
 
         vector<int> dx = {0,1,0,-1};
         vector<int> dy = {1,0,-1,0};
+        int tm = 0;
 
         while(!q.empty()){
-
-            int size = q.size();
-            time++;
-
-            for(int k=0;k<size;k++){
-                int r = q.front().first;
-                int c = q.front().second;
-                q.pop();
-                for(int i=0;i<4;i++){
-                    int nr = r + dx[i];
-                    int nc = c + dy[i];
-                    if(nr>=0 && nr<n && nc>=0 && nc<m && vis[nr][nc]!=2 && grid[nr][nc]==1){
-                        q.push({nr,nc});
-                        vis[nr][nc]=2;
-                        rotten++;
-                    }
-                }     
-            }       
-
+            int r = q.front().first.first;
+            int c = q.front().first.second;
+            int t = q.front().second;
+            tm = max(tm,t);
+            q.pop();
+            for(int i=0;i<4;i++){
+                int nr = r + dx[i];
+                int nc = c + dy[i];
+                if(nr>=0 && nr<n && nc>=0 && nc<m && grid[nr][nc]==1 && vis[nr][nc]!=2){
+                    vis[nr][nc]=2;
+                    q.push({{nr,nc},t+1});
+                    rotten++;
+                }
+            }
         }
-
-        if(fresh==0){
-            return 0;
-        }
-
-        return rotten==fresh?time-1:-1;
+        return fresh==rotten?tm:-1;
 
     }
 };
