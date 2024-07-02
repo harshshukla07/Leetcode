d3ecb2c9800a9928f5c7a2cc8ba5ca7eabc4fdd7
Harsh Shukla <107511647+harshshukla07@users.noreply.github.com>
2024-07-02 13:38:57 +0530

    Time: 7 ms (92.21%) | Memory: 22.2 MB (44.93%) - LeetSync

--- 543-diameter-of-binary-tree/diameter-of-binary-tree.cpp
+++ 543-diameter-of-binary-tree/diameter-of-binary-tree.cpp
@@ -6,29 +6,30 @@
  *     TreeNode *right;
  *     TreeNode() : val(0), left(nullptr), right(nullptr) {}
  *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
- *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+ *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left),
+ * right(right) {}
  * };
  */
 class Solution {
 
 private:
-    int solve(TreeNode* root, int& ans){
-        if(root==NULL){
+    int solve(TreeNode* root, int& ans) {
+        if (root == NULL) {
             return 0;
         }
 
-        int lh = solve(root->left,ans);
-        int rh = solve(root->right,ans);
+        int lh = solve(root->left, ans);
+        int rh = solve(root->right, ans);
 
-        ans = max(ans,lh+rh);
+        ans = max(ans, lh + rh);
 
-        return 1 + max(lh,rh);
+        return 1 + max(lh, rh);
     }
 
 public:
     int diameterOfBinaryTree(TreeNode* root) {
         int ans = 0;
-        solve(root,ans);
+        solve(root, ans);
         return ans;
     }
 };
