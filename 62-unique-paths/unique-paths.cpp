2052af6c3f8af74575d7f9a1b46260792e34d30d
Harsh Shukla <107511647+harshshukla07@users.noreply.github.com>
2024-08-01 12:59:53 +0530

    Time: 0 ms (100.00%) | Memory: 8.1 MB (8.28%) - LeetSync

--- 62-unique-paths/unique-paths.cpp
+++ 62-unique-paths/unique-paths.cpp
@@ -1,72 +1,27 @@
-//          TOP DOWN APPROACH
+class Solution {
 
-
-// class Solution {
-
-// private:
-//     int helper(int i, int j, vector<vector<int>>& dp){
-//         if(i<0 || j<0){
-//             return 0;
-//         }
-
-//         if(i==0 && j==0){
-//             return 1;
-//         }
-
-//         if(dp[i][j]!=-1){
-//             return dp[i][j];
-//         }
-
-//         int up = 0;
-//         if(i>0){
-//             up = helper(i-1,j,dp);
-//         }
-//         int left = 0;
-//         if(j>0){
-//             left = helper(i,j-1,dp);
-//         }
-
-//         return dp[i][j] = up + left;
-//     }
-
-// public:
-//     int uniquePaths(int m, int n) {
-//         vector<vector<int>>dp(m,vector<int>(n,-1));
-//         helper(m-1,n-1,dp);
-//         return dp[m-1][n-1];
-//     }
-// };
-
-
-
-//      BOTTOM UP APPROACH
-
-class Solution{
-
-    public:
-    int uniquePaths(int m, int n){
-        vector<vector<int>>dp(m,vector<int>(n,0));
-        dp[0][0]=1;
-        for(int i=0;i<m;i++){
-            for(int j=0;j<n;j++){
-                if(i==0 && j==0){
-                    dp[i][j]=1;
-                }
-                else{
-                    int up=0;
-                if(i>0){
-                    up = dp[i-1][j];
-                }
-                int left = 0;
-                if(j>0){
-                    left = dp[i][j-1];
-                }
-
-                dp[i][j] = up + left;
-                }
-            }
+private:
+    int solve(int m, int n, vector<vector<int>>& dp ){
+        if(m<0 || n<0){
+            return 0;
         }
-        return dp[m-1][n-1];
+        if(m==0 && n==0){
+            return 1;
+        }
+
+        if(dp[m][n]!=-1){
+            return dp[m][n];
+        }
+
+        int up = solve(m-1,n,dp);
+        int left = solve(m,n-1,dp);
+
+        return dp[m][n] = up + left;
     }
 
+public:
+    int uniquePaths(int m, int n) {
+        vector<vector<int>> dp(m,vector<int>(n,-1));
+        return solve(m-1,n-1,dp);
+    }
 };
